dfc62a76c8f730132fdb57e81dd460cb084d7621
Reid Bandy <reidbandy@gmail.com>
2024-01-08 14:47:18 -0500

    perf

---
+++ perf.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+./target/debug/game &
+pid=$!
+echo "pid: $pid"
+perf record -gbz -p "$pid" --call-graph fp &
+sleep 30
+kill "$pid"
+sleep 1
+~/.cargo/bin/flamegraph --perfdata perf.data --flamechart
+google-chrome-stable flamegraph.svg
